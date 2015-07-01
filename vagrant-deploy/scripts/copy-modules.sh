9f3332a3e9ea5cc0d4eeb478d4646116af995e25
Hemanth S.R <hemanthfeb25@gmail.com>
2015-07-01 17:47:10 +0530

    preethi, Hmeanth | changed the db user in application properties.

--- vagrant-deploy/scripts/copy-modules.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-scp bahmnicore-omod/target/bahmnicore-*-SNAPSHOT.omod root@XXXX.XXXX.XXXX.XXXX:/home/jss/.OpenMRS/modules
-scp openerp-atomfeed-client-omod/target/openerp-atomfeed-client-omod-*.omod root@XXXX.XXXX.XXXX.XXXX:/home/jss/.OpenMRS/modules
-scp reference-data/omod/target/reference-data-*.omod root@XXXX.XXXX.XXXX.XXXX:/home/jss/.OpenMRS/modules
