d66bbb7ad92bcc6ff44d8116fc8b1a60ffc8bf57
Sudhakar <sudhakar82@gmail.com>
2015-07-13 17:08:41 +0530

    Sudhakar, Preethi | #2508 | Added integration tests and removed the getFillerOrderNumber setting (placeOrderNumber is enough)

--- pacs-integration-webapp/src/test/resources/insertModalities.sql
+++ pacs-integration-webapp/src/test/resources/insertModalities.sql
@@ -1,1 +1,2 @@
-INSERT INTO modality (id, name, description, ip, port, timeout) values (1, 'X-Ray', 'X ray machine', 'XXXX.XXXX.XXXX.XXXX', 9001, 10);
+-- modality stub server uses the same port and timeout inserted here
+INSERT INTO modality (id, name, description, ip, port, timeout) values (1, 'X-Ray', 'X ray machine', '127.0.0.1', 6001, 2000);
